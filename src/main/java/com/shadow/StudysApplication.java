4a5397cba616070bc297b82eb4860ca52198dcb0
Eduardo Santos Gomes ferreira <106843885+EspadachiNNegr0@users.noreply.github.com>
2024-11-13 21:12:16 -0300

    Spring commit

---
+++ src/main/java/com/shadow/StudysApplication.java
@@ -0,0 +1,15 @@
+package com.shadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudysApplication.class, args);
+	}
+}
+
+
+
