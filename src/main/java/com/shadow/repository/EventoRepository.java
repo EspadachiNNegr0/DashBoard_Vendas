aab98209eba876d8e6ba4efb8266f9e0cf0de647
Eduardo Santos Gomes ferreira <106843885+EspadachiNNegr0@users.noreply.github.com>
2024-11-17 18:27:49 -0300

    Delete src directory

--- src/main/java/com/shadow/repository/EventoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shadow.repository;
-
-import com.shadow.models.Evento;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventoRepository extends CrudRepository<Evento, String> {
-}
